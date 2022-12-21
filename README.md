8ecdc01e8144829c4e798312cd03a6bf4e6675b1
Bogdan <1605030@gmail.com>
2022-12-21 11:56:46 +0200

    Setting start position, animation of pieces.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# My-comfortable-super-chess
-I like chess and I am going to play chess time to time. So it is a tool for this activity!
+DESCRIPTION OF THE WEB-PROJECT "MY COMFORTABLE CHESS"
+
+I created this page for my friends and my own pleasure, but it also shows my abilities (untill Dec 2022). The idea, design, technical implementation are mine).
+
+///// Used techs and tools /////
+
+Web Design | HTML | CSS | JavaScript
+
+///// Features /////
+
+There are much grid technology).
+
+No optimization for mobile devices (at this time).
+
+Part of the project are under construction but you realy can play chess). Just open the page and reed a description!
--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+///////////////////////////////////////////////////////
+// DRAG & DROP PIESES
+
 // set class "piece" and allow drag attribute to all piece elements
 const allPieces = document.querySelectorAll(
   "div.side-board-black > div > img,  div.side-board-white > div > img"
@@ -38,3 +41,107 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
   }
 }
+
+///////////////////////////////////////////////////////
+// CONTROL BUTTONS
+
+const buttonStart = document.querySelector(".btn-start");
+console.log(buttonStart);
+buttonStart.addEventListener("click", function (e) {
+  document
+    .getElementById("a8")
+    .appendChild(document.getElementById("black-rook-1"));
+  document
+    .getElementById("b8")
+    .appendChild(document.getElementById("black-knight-1"));
+  document
+    .getElementById("c8")
+    .appendChild(document.getElementById("black-bishop-1"));
+  document
+    .getElementById("d8")
+    .appendChild(document.getElementById("black-queen"));
+  document
+    .getElementById("e8")
+    .appendChild(document.getElementById("black-king"));
+  document
+    .getElementById("f8")
+    .appendChild(document.getElementById("black-bishop-2"));
+  document
+    .getElementById("g8")
+    .appendChild(document.getElementById("black-knight-2"));
+  document
+    .getElementById("h8")
+    .appendChild(document.getElementById("black-rook-2"));
+  document
+    .getElementById("a7")
+    .appendChild(document.getElementById("black-pawn-1"));
+  document
+    .getElementById("b7")
+    .appendChild(document.getElementById("black-pawn-2"));
+  document
+    .getElementById("c7")
+    .appendChild(document.getElementById("black-pawn-3"));
+  document
+    .getElementById("d7")
+    .appendChild(document.getElementById("black-pawn-4"));
+  document
+    .getElementById("e7")
+    .appendChild(document.getElementById("black-pawn-5"));
+  document
+    .getElementById("f7")
+    .appendChild(document.getElementById("black-pawn-6"));
+  document
+    .getElementById("g7")
+    .appendChild(document.getElementById("black-pawn-7"));
+  document
+    .getElementById("h7")
+    .appendChild(document.getElementById("black-pawn-8"));
+  document
+    .getElementById("a2")
+    .appendChild(document.getElementById("white-pawn-1"));
+  document
+    .getElementById("b2")
+    .appendChild(document.getElementById("white-pawn-2"));
+  document
+    .getElementById("c2")
+    .appendChild(document.getElementById("white-pawn-3"));
+  document
+    .getElementById("d2")
+    .appendChild(document.getElementById("white-pawn-4"));
+  document
+    .getElementById("e2")
+    .appendChild(document.getElementById("white-pawn-5"));
+  document
+    .getElementById("f2")
+    .appendChild(document.getElementById("white-pawn-6"));
+  document
+    .getElementById("g2")
+    .appendChild(document.getElementById("white-pawn-7"));
+  document
+    .getElementById("h2")
+    .appendChild(document.getElementById("white-pawn-8"));
+  document
+    .getElementById("a1")
+    .appendChild(document.getElementById("white-rook-1"));
+  document
+    .getElementById("b1")
+    .appendChild(document.getElementById("white-knight-1"));
+  document
+    .getElementById("c1")
+    .appendChild(document.getElementById("white-bishop-1"));
+  document
+    .getElementById("d1")
+    .appendChild(document.getElementById("white-queen"));
+  document
+    .getElementById("e1")
+    .appendChild(document.getElementById("white-king"));
+  document
+    .getElementById("f1")
+    .appendChild(document.getElementById("white-bishop-2"));
+  document
+    .getElementById("g1")
+    .appendChild(document.getElementById("white-knight-2"));
+  document
+    .getElementById("h1")
+    .appendChild(document.getElementById("white-rook-2"));
+});
