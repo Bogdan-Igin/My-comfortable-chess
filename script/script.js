48e638a317d467e9a3e0f5af3ec114a3f32b430d
Bogdan <1605030@gmail.com>
2023-05-09 21:39:39 +0300

    Move pieses to and from main board + page "About"

--- script/script.js
+++ script/script.js
@@ -1,3 +1,111 @@
+"use strict";
+
+////////////////
+// UNDER CONSTRUCTION
+
+const underConstr = document.querySelectorAll(".under-constr");
+underConstr.forEach(item => {
+  item.addEventListener("click", () => {
+    alert("Sorry, this item under construction");
+  });
+});
+
+////////////////
+// LEFT SIDE BUTTONS
+
+////
+// "All to the board" & "All from the board" buttons
+
+// start positions array
+const startPositions = [
+  {a8: "black-rook-1"},
+  {b8: "black-knight-1"},
+  {c8: "black-bishop-1"},
+  {d8: "black-queen"},
+  {e8: "black-king"},
+  {f8: "black-bishop-2"},
+  {g8: "black-knight-2"},
+  {h8: "black-rook-2"},
+  {a7: "black-pawn-1"},
+  {b7: "black-pawn-2"},
+  {c7: "black-pawn-3"},
+  {d7: "black-pawn-4"},
+  {e7: "black-pawn-5"},
+  {f7: "black-pawn-6"},
+  {g7: "black-pawn-7"},
+  {h7: "black-pawn-8"},
+  {a2: "white-pawn-1"},
+  {b2: "white-pawn-2"},
+  {c2: "white-pawn-3"},
+  {d2: "white-pawn-4"},
+  {e2: "white-pawn-5"},
+  {f2: "white-pawn-6"},
+  {g2: "white-pawn-7"},
+  {h2: "white-pawn-8"},
+  {a1: "white-rook-1"},
+  {b1: "white-knight-1"},
+  {c1: "white-bishop-1"},
+  {d1: "white-queen"},
+  {e1: "white-king"},
+  {f1: "white-bishop-2"},
+  {g1: "white-knight-2"},
+  {h1: "white-rook-2"},
+]
+// side position array
+const sidePositions = [
+  {"a8-side": "black-rook-1"},
+  {"b8-side": "black-knight-1"},
+  {"c8-side": "black-bishop-1"},
+  {"d8-side": "black-queen"},
+  {"e8-side": "black-king"},
+  {"f8-side": "black-bishop-2"},
+  {"g8-side": "black-knight-2"},
+  {"h8-side": "black-rook-2"},
+  {"a7-side": "black-pawn-1"},
+  {"b7-side": "black-pawn-2"},
+  {"c7-side": "black-pawn-3"},
+  {"d7-side": "black-pawn-4"},
+  {"e7-side": "black-pawn-5"},
+  {"f7-side": "black-pawn-6"},
+  {"g7-side": "black-pawn-7"},
+  {"h7-side": "black-pawn-8"},
+  {"a2-side": "white-pawn-1"},
+  {"b2-side": "white-pawn-2"},
+  {"c2-side": "white-pawn-3"},
+  {"d2-side": "white-pawn-4"},
+  {"e2-side": "white-pawn-5"},
+  {"f2-side": "white-pawn-6"},
+  {"g2-side": "white-pawn-7"},
+  {"h2-side": "white-pawn-8"},
+  {"a1-side": "white-rook-1"},
+  {"b1-side": "white-knight-1"},
+  {"c1-side": "white-bishop-1"},
+  {"d1-side": "white-queen"},
+  {"e1-side": "white-king"},
+  {"f1-side": "white-bishop-2"},
+  {"g1-side": "white-knight-2"},
+  {"h1-side": "white-rook-2"},
+]
+
+// moving pieses to the start position function
+function allToPosition(positionsArray) {
+  positionsArray.forEach(item => {
+    let key = Object.keys(item)[0];
+    let value = item[key];
+    document.getElementById(key).appendChild(document.getElementById(value));
+  })
+}
+
+// "All to the board"
+const buttonAllToBoard = document.querySelector(".buttons-section-left button:nth-child(3)");
+buttonAllToBoard.addEventListener("click", () => allToPosition(startPositions));
+
+// "All from the board"
+const buttonAllFromBoard = document.querySelector(".buttons-section-left button:nth-child(4)");
+console.log(buttonAllFromBoard);
+buttonAllFromBoard.addEventListener("click", () => allToPosition(sidePositions));
+
+
 ////////////////
 // DRAG & DROP PIESES
 
@@ -50,6 +158,7 @@ function drop(ev) {
       illegalMove = false;
     } else {
       ev.target.appendChild(document.getElementById(data));
+      
     }
   }
 }
@@ -104,61 +213,5 @@ function noteMoves(moved, target) {
   }
 }
 
-////
-// Setting Castl
 
 
-
-
-////////////////
-// CONTROL BUTTONS
-
-////
-// "Start" button
-
-// Start positions object
-const startPosition = [
-  {a8: "black-rook-1"},
-  {b8: "black-knight-1"},
-  {c8: "black-bishop-1"},
-  {d8: "black-queen"},
-  {e8: "black-king"},
-  {f8: "black-bishop-2"},
-  {g8: "black-knight-2"},
-  {h8: "black-rook-2"},
-  {a7: "black-pawn-1"},
-  {b7: "black-pawn-2"},
-  {c7: "black-pawn-3"},
-  {d7: "black-pawn-4"},
-  {e7: "black-pawn-5"},
-  {f7: "black-pawn-6"},
-  {g7: "black-pawn-7"},
-  {h7: "black-pawn-8"},
-  {a2: "white-pawn-1"},
-  {b2: "white-pawn-2"},
-  {c2: "white-pawn-3"},
-  {d2: "white-pawn-4"},
-  {e2: "white-pawn-5"},
-  {f2: "white-pawn-6"},
-  {g2: "white-pawn-7"},
-  {h2: "white-pawn-8"},
-  {a1: "white-rook-1"},
-  {b1: "white-knight-1"},
-  {c1: "white-bishop-1"},
-  {d1: "white-queen"},
-  {e1: "white-king"},
-  {f1: "white-bishop-2"},
-  {g1: "white-knight-2"},
-  {h1: "white-rook-2"},
-]
-
-const buttonStart = document.querySelector(".btn-start");
-
-// handle "click" on start button
-buttonStart.addEventListener("click", function (e) {
-  startPosition.forEach(item => {
-    let key = Object.keys(item)[0];
-    let value = item[key];
-    document.getElementById(key).appendChild(document.getElementById(value));
-  })
-});
